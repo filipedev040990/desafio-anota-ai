8ac7a5bc44051a5c93f3424b7701bf6a9372574a
Filipe Siqueira <filipedev040990@gmail.com>
2024-06-26 17:52:59 -0300

    chore: initial commit

---
+++ src/shared/helpers/http.helper.ts
@@ -0,0 +1,26 @@
+import { HttpResponse } from '@/shared/types'
+import { logger } from './logger.helper'
+
+export const success = (statusCode: number, body: any): HttpResponse => ({
+  statusCode,
+  body
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: {
+    error: error.name,
+    message: error.message
+  }
+})
+
+export const serverError = (error: Error): HttpResponse => {
+  logger.error(error)
+  return {
+    statusCode: 500,
+    body: {
+      error: error.name,
+      message: error.message
+    }
+  }
+}
