97ddd70a852758fb1a093222e9963d80e49e0ef0
Filipe Siqueira <filipedev040990@gmail.com>
2024-06-29 19:29:52 -0300

    feat: add document validation

--- src/application/usecases/owner/create-owner.usecase.spec.ts
+++ src/application/usecases/owner/create-owner.usecase.spec.ts
@@ -2,6 +2,7 @@ import { OwnerData, OwnerEntity } from '@/domain/entities/owner/owner.entitt'
 import { CreateOwnerUseCase } from './create-owner.usecase'
 import { OwnerRepositoryInterface } from '@/domain/interfaces/repositories/owner-repository.interface'
 import { mock } from 'jest-mock-extended'
+import { InvalidParamError } from '@/shared/errors'
 
 const repository = mock<OwnerRepositoryInterface>()
 const fakeOwnerEntity = {
@@ -20,10 +21,11 @@ describe('CreateOwnerUseCase', () => {
     sut = new CreateOwnerUseCase(repository)
     input = {
       name: 'AnyName',
-      document: 'AnyDocument'
+      document: 'anyDocument'
     }
     jest.spyOn(OwnerEntity, 'build').mockReturnValue(fakeOwnerEntity)
     repository.save.mockResolvedValue(fakeOwnerEntity)
+    repository.getByDocument.mockResolvedValue(null)
   })
 
   test('should call OwnerEntity once and with correct values', async () => {
@@ -42,5 +44,17 @@ describe('CreateOwnerUseCase', () => {
   test('should return a correct output', async () => {
     const output = await sut.execute(input)
     expect(output).toEqual({ id: 'anyId' })
+  })
+
+  test('should call OwnerRepository.getByDocument once and with correct document', async () => {
+    await sut.execute(input)
+    expect(repository.getByDocument).toHaveBeenCalledTimes(1)
+    expect(repository.getByDocument).toHaveBeenCalledWith('anyDocument')
+  })
+
+  test('should throw if document already exists', async () => {
+    repository.getByDocument.mockResolvedValueOnce(fakeOwnerEntity)
+    const promise = sut.execute(input)
+    await expect(promise).rejects.toThrowError(new InvalidParamError('This document already exists'))
   })
 })
--- src/application/usecases/owner/create-owner.usecase.ts
+++ src/application/usecases/owner/create-owner.usecase.ts
@@ -1,10 +1,17 @@
 import { OwnerData, OwnerEntity } from '@/domain/entities/owner/owner.entitt'
 import { OwnerRepositoryInterface } from '@/domain/interfaces/repositories/owner-repository.interface'
 import { CreateOwnerUseCaseInterface } from '@/domain/interfaces/usecases/owner/create-owner-usecase.interface'
+import { InvalidParamError } from '@/shared/errors'
 
 export class CreateOwnerUseCase implements CreateOwnerUseCaseInterface {
   constructor (private readonly repository: OwnerRepositoryInterface) {}
   async execute (input: OwnerData): Promise<{ id: string }> {
+    const documentExists = await this.repository.getByDocument(input?.document)
+
+    if (documentExists) {
+      throw new InvalidParamError('This document already exists')
+    }
+
     const owner = OwnerEntity.build(input)
     await this.repository.save(owner)
     return { id: owner.id }
