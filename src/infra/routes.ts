3946c95dc8763bc3766146a1725274f6f4d03bd7
Filipe Siqueira <filipedev040990@gmail.com>
2024-07-03 19:47:18 -0300

    feat: add create category route

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -1,3 +1,4 @@
+import { makeCreateCategoryControllerFactory } from './factories/controllers/create-category-controller.factory'
 import { makeCreateOwnerControllerFactory } from './factories/controllers/create-owner-controller.factory'
 import { expressRouteAdapter } from './tools/express'
 import { Router } from 'express'
@@ -5,5 +6,6 @@ import { Router } from 'express'
 const router = Router()
 
 router.post('/owner', expressRouteAdapter(makeCreateOwnerControllerFactory()))
+router.post('/category', expressRouteAdapter(makeCreateCategoryControllerFactory()))
 
 export { router }
