b986fd77c5090b4899c03a790f29ff38cad4e47b
Filipe Siqueira <filipedev040990@gmail.com>
2024-07-07 10:08:30 -0300

    feat: add catalog entity

---
+++ src/domain/entities/catalog/catalog.entity.spec.ts
@@ -0,0 +1,64 @@
+import { MissingParamError } from '@/shared/errors'
+import { CatalogData, CatalogEntity } from './catalog.entity'
+import MockDate from 'mockdate'
+
+jest.mock('crypto', () => ({
+  randomUUID: jest.fn().mockReturnValue('anyId')
+}))
+
+describe('CatalogEntity', () => {
+  let sut: any
+  let input: any
+
+  beforeEach(() => {
+    sut = CatalogEntity
+    input = {
+      ownerId: 'anyOwnerId',
+      categoryId: 'AnyTitle',
+      items: [
+        {
+          title: 'Product 1',
+          description: 'Product 1',
+          price: 1800
+        }, {
+          title: 'Product 2',
+          description: 'Product 2',
+          price: 1000
+        }]
+    }
+  })
+
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('should throw if any required field is not provided', async () => {
+    const requiredFields: Array<keyof CatalogData> = ['ownerId', 'categoryId']
+
+    for (const field of requiredFields) {
+      input[field] = undefined
+
+      expect(() => {
+        sut.build(input)
+      }).toThrowError(new MissingParamError(field))
+
+      input[field] = field
+    }
+  })
+
+  test('should make a correct Category', async () => {
+    const category = sut.build(input)
+    expect(category).toEqual({
+      id: 'anyId',
+      ownerId: 'anyOwnerId',
+      categoryId: 'AnyTitle',
+      items: input.items,
+      createdAt: new Date(),
+      updatedAt: null
+    })
+  })
+})
---
+++ src/domain/entities/catalog/catalog.entity.ts
@@ -0,0 +1,47 @@
+import { MissingParamError } from '@/shared/errors'
+import { randomUUID } from 'crypto'
+
+export type CatalogData = {
+  ownerId: string
+  categoryId: string
+  items: Item []
+}
+
+export type Item = {
+  title: string
+  description: string
+  price: number
+}
+
+export class CatalogEntity {
+  constructor (
+    public readonly id: string,
+    public readonly ownerId: string,
+    public readonly categoryId: string,
+    public readonly items: Item [],
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date | null
+  ) {}
+
+  public static build (input: CatalogData): CatalogEntity {
+    this.validate(input)
+    return this.create(input)
+  }
+
+  private static validate (input: CatalogData): void {
+    const requiredFields: Array<keyof CatalogData> = ['ownerId', 'categoryId']
+    for (const field of requiredFields) {
+      if (!input[field]) {
+        throw new MissingParamError(field)
+      }
+    }
+
+    if (input?.items.length < 1) {
+      throw new MissingParamError('items')
+    }
+  }
+
+  private static create (input: CatalogData): CatalogEntity {
+    return new CatalogEntity(randomUUID(), input.ownerId, input.categoryId, input.items, new Date(), null)
+  }
+}
