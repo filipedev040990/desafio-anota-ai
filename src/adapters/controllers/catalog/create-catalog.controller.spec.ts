a08875041f0ecba702bf1093344ec8fa25bca7ff
Filipe Siqueira <filipedev040990@gmail.com>
2024-07-08 13:59:33 -0300

    feat: add create catalog controller

---
+++ src/adapters/controllers/catalog/create-catalog.controller.spec.ts
@@ -0,0 +1,47 @@
+import { HttpRequest } from '@/shared/types'
+import { InvalidParamError } from '@/shared/errors'
+import { badRequest } from '@/shared/helpers/http.helper'
+import { mock } from 'jest-mock-extended'
+import { CreateCatalogUseCaseInterface } from '@/domain/interfaces/usecases/catalog/create-catalog-usecase.interface'
+import { CreateCatalogController } from './create-catalog.controller'
+
+const usecase = mock<CreateCatalogUseCaseInterface>()
+
+describe('CreateCatalogController', () => {
+  let sut: CreateCatalogController
+  let input: HttpRequest
+
+  beforeEach(() => {
+    sut = new CreateCatalogController(usecase)
+    input = {
+      body: {
+        ownerId: 'anyOwnerId',
+        categoryId: 'anyCategoryId',
+        items: ['Product1Id', 'Product2Id']
+      }
+    }
+    usecase.execute.mockResolvedValue({ id: 'anyCatalogId' })
+  })
+
+  test('should call CreateCatalogUseCase once and with correct values', async () => {
+    await sut.execute(input)
+    expect(usecase.execute).toHaveBeenCalledTimes(1)
+    expect(usecase.execute).toHaveBeenCalledWith(input.body)
+  })
+
+  test('should return a correct output', async () => {
+    const output = await sut.execute(input)
+    expect(output).toEqual({ statusCode: 201, body: { id: 'anyCatalogId' } })
+  })
+
+  test('should return a correct error if CreateOwnerUseCase throws', async () => {
+    const error = new InvalidParamError('anyParam')
+    usecase.execute.mockImplementationOnce(() => {
+      throw error
+    })
+
+    const output = await sut.execute(input)
+
+    expect(output).toEqual(badRequest(error))
+  })
+})
