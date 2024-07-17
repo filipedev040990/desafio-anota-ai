0ae38adc9cdae93729dafc240eab9b7672d9083e
Filipe Siqueira <filipedev040990@gmail.com>
2024-07-17 19:03:55 -0300

    feat: add update owner repository

--- src/infra/database/repositories/owner.repository.ts
+++ src/infra/database/repositories/owner.repository.ts
@@ -1,5 +1,6 @@
 import { OwnerRepositoryData, OwnerRepositoryInterface } from '@/domain/interfaces/repositories/owner-repository.interface'
 import { prismaClient } from '../prisma-client'
+import { UpdateOwnerInput } from '@/domain/interfaces/usecases/owner/update-owner-usecase.interface'
 
 export class OwnerRepository implements OwnerRepositoryInterface {
   async save (input: OwnerRepositoryData): Promise<OwnerRepositoryData> {
@@ -21,5 +22,25 @@ export class OwnerRepository implements OwnerRepositoryInterface {
 
   async getById (id: string): Promise<OwnerRepositoryData | null> {
     return await prismaClient.owner.findFirst({ where: { id } })
+  }
+
+  async update (input: UpdateOwnerInput): Promise<void> {
+    const { id, name, document, password } = input
+
+    const data: UpdateOwnerInput = { id }
+
+    if (name) {
+      data.name = name
+    }
+
+    if (document) {
+      data.document = document
+    }
+
+    if (password) {
+      data.password = password
+    }
+
+    await prismaClient.owner.update({ where: { id }, data })
   }
 }
